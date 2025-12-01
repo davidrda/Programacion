00c746ffbd0bd873e315240225b51e1131643835
David Ramírez <davidramirez.rda@gmail.com>
2025-12-01 12:03:29 +0100

    update

--- T4/EjerciciosArraysMultidimensionales/src/Ejercicio7/Controller.java
+++ T4/EjerciciosArraysMultidimensionales/src/Ejercicio7/Controller.java
@@ -2,6 +2,7 @@ package Ejercicio7;
 
 public class Controller {
     private int[][] matriz = new int[3][4];
+    private int[][] matrizTranspuesta = new int[4][3];
 
     // Se genera los números de matriz
     public void ejercicio(){
@@ -11,11 +12,14 @@ public class Controller {
             }
         }
         // Una vez generados, el metod0 se puede imprimir ya fuera del bucle
-        imprimirMatriz();
+        imprimirMatrizOriginal();
+        transponerMatriz();
+        imprimirMatrizTranspuesta();
     }
 
     // Se crea el metod0 para imprimir
-    public void imprimirMatriz(){
+    public void imprimirMatrizOriginal(){
+        System.out.println("Matriz original:");
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
                 System.out.print(matriz[i][j] + "\t");
@@ -25,8 +29,22 @@ public class Controller {
     }
 
     public void transponerMatriz(){
+        // Transponemos: se intercambian filas por columnas
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {}
+            for (int j = 0; j < matriz[i].length; j++) {
+                matrizTranspuesta[j][i] = matriz[i][j];
+                // fila, columna -> columna, fila
+            }
+        }
+    }
+
+    public void imprimirMatrizTranspuesta(){
+        System.out.println("Matriz transpuesta:");
+        for (int i = 0; i < matrizTranspuesta.length; i++) {
+            for (int j = 0; j < matrizTranspuesta[i].length; j++) {
+                System.out.print(matrizTranspuesta[i][j] + "\t");
+            }
+            System.out.println();
         }
     }
 
--- T4/EjerciciosArraysMultidimensionales/src/Ejercicio7/Main.java
+++ T4/EjerciciosArraysMultidimensionales/src/Ejercicio7/Main.java
@@ -3,5 +3,8 @@ package Ejercicio7;
 public class Main {
     public static void main(String[] args) {
 
+        Controller c = new Controller();
+        c.ejercicio();
+
     }
 }
